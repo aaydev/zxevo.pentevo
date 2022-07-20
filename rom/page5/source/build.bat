314afd9700916a99f1b8d47cb78d991afa2fc1ad
savelij <savelij@c6c76956-fad2-4150-a3a9-ea0a427205ba>
2022-07-20 07:20:02 +0000

    ERS 0.59.02: - исправлено не правильный возврат типа FAT после инициализации переменных раздела. - исправлено не выставление типа FAT при отсутствии MBR.
    git-svn-id: svn://localhost/local-pentevo@1010 c6c76956-fad2-4150-a3a9-ea0a427205ba

--- rom/page5/source/build.bat
+++ rom/page5/source/build.bat
@@ -12,8 +12,5 @@ cd ..\..\page5\source
 ..\..\..\tools\mhmt\mhmt -mlz atm_code.fnt atm_code_pack.bin
 ..\..\..\tools\mhmt\mhmt -mlz perfpack.bin perfpack_pack.bin
 
-..\..\..\tools\asl\bin\asl -U -L -x -olist rst8service.lst -D DELVAR=0 rst8service.a80
-..\..\..\tools\asl\bin\p2bin rst8service.p ..\rst8service.rom -r $-$ -k
-
 ..\..\..\tools\asl\bin\asl -U -L -x -olist rst8service_fe.lst -D DOS_FE,DELVAR=0 rst8service.a80
 ..\..\..\tools\asl\bin\p2bin rst8service.p ..\rst8service_fe.rom -r $-$ -k
