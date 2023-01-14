9700473c665fedd150d3cd31fb10e78e1299838f
lvd <lvd@c6c76956-fad2-4150-a3a9-ea0a427205ba>
2023-01-14 23:01:18 +0000

    sdload: mkfont,py
    git-svn-id: svn://localhost/local-pentevo@1051 c6c76956-fad2-4150-a3a9-ea0a427205ba

--- fpga/sdload/trunk/video/mkfont.py
+++ fpga/sdload/trunk/video/mkfont.py
@@ -27,12 +27,58 @@
 
 import argparse,os,sys
 
+class ZXPic:
+
+	def __init__(self,filename):
+		
+		with open(filename,'rb') as file:
+			self.zxscr = bytes(file.read())
+
+		if( len(self.zxscr)!=6144 and len(self.zxscr)!=6192 ):
+			sys.exit('Wrong zx file <{}> size, must be 6144 or 6912'.format(filename))
+
+		if( len(self.zxscr)==6912 ):
+			self.colored = True
+		else:
+			self.colored = False
+
+		self.pixels = bytes(self.zxscr[:6144])
+
+		if( self.colored ):
+			self.attrs = bytes(self.zxscr[6144:])
+		else:
+			self.attrs = None
+
+
+	def get_pixel(self,x,y):
+		
+		if( x<0 or x>255 or y<0 or y>191 ):
+			sys.exit('x,y must be within 0..255 and 0..191 range!')
+
+
+		bitnum = 7 - (x & 7)
+
+		offset = (x>>3) + (y & 7)*256 + ((y & 0x38)>>3)*32 + ((y & 0xC0)>>6)*2048
+
+		return True if self.pixels[offset] & (1<<bitnum) else False
+
+
 
 
 
 
 
 def main():
+
+	# parse arguments
+	p = argparse.ArgumentParser()
+	#
+	p.add_argument('--scr', '-s',           action='store', help='6912 or 6144 ZX screen with font')
+	p.add_argument('--out', '-o',           action='store', help='Filename prefix for resulting file(s). Extensions will be added as needed')
+	p.add_argument(         '-x', type=int, action='store', help='Initial X position of 8x8 block with first symbol (that must be a space)')
+	p.add_argument(         '-y', type=int, action='store', help='Initial Y position of 8x8 block with first symbol (that must be a space)')
+	#
+	args = p.parse_args()
 
 
 if __name__=="__main__":
