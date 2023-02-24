b7ce4304b3072d390956d2732f02b86d0fe3980e
savelij <savelij@c6c76956-fad2-4150-a3a9-ea0a427205ba>
2023-02-24 16:24:05 +0000

    ERS 0.59.11: - код флешера передвинут по адресам выше 0x8000. - при размере TRD образа больше или меньше стандартныx 640к загрузка в рамдиск без запроса. - добавлен пункт демонтирования всех смонтированных образов.
    git-svn-id: svn://localhost/local-pentevo@1067 c6c76956-fad2-4150-a3a9-ea0a427205ba

--- rom/mainmenu/src/build.bat
+++ rom/mainmenu/src/build.bat
@@ -1,18 +1,23 @@
 
 echo off
 
-..\..\..\tools\asl\bin\asl -U -L -x -olist main.lst main.a80
-..\..\..\tools\asl\bin\p2bin main.p main.rom -r $-$ -k
+if not exist rom md rom
+if not exist lst md lst
+set path=d:\yad\svn\pentevo\tools\asl\bin\;d:\yad\svn\pentevo\tools\mhmt\
+set includes=d:\yad\svn\pentevo\rom
 
-..\..\..\tools\asl\bin\asl -U -L -x -olist main_fe.lst -D DOS_FE main.a80
-..\..\..\tools\asl\bin\p2bin main.p main_fe.rom -r $-$ -k
+asl -U -L -x -olist lst\main.lst -i %includes% main.a80
+p2bin main.p rom\main.rom -r $-$ -k
 
-..\..\..\tools\asl\bin\asl -U -L cmosset.a80
-..\..\..\tools\asl\bin\p2bin cmosset.p cmosset.rom -r $-$ -k
+asl -U -L -x -olist lst\main_fe.lst -i %includes% -D DOS_FE main.a80
+p2bin main.p rom\main_fe.rom -r $-$ -k
 
-..\..\..\tools\mhmt\mhmt -mlz main.rom ..\main_pack.rom
-..\..\..\tools\mhmt\mhmt -mlz main_fe.rom ..\main_fe_pack.rom
-..\..\..\tools\mhmt\mhmt -mlz cmosset.rom ..\cmosset_pack.rom
-..\..\..\tools\mhmt\mhmt -mlz chars_eng.bin ..\chars_pack.rom
+asl -U -L -x -olist lst\cmosset.lst -i %includes% cmosset.a80
+p2bin cmosset.p rom\cmosset.rom -r $-$ -k
+
+mhmt -mlz rom\main.rom ..\main_pack.rom
+mhmt -mlz rom\main_fe.rom ..\main_fe_pack.rom
+mhmt -mlz rom\cmosset.rom ..\cmosset_pack.rom
+mhmt -mlz rom\chars_eng.bin ..\chars_pack.rom
 
 rem del *.rom
