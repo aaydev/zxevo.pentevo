54b60ea1e4783b4dc25377187dbf1ffdb6450f26
dimkam <dimkam@c6c76956-fad2-4150-a3a9-ea0a427205ba>
2023-09-24 13:39:46 +0000

    AVR: некоторые Sega-Pad'ы детектились как 8ми кнопочные
    git-svn-id: svn://localhost/local-pentevo@1098 c6c76956-fad2-4150-a3a9-ea0a427205ba

--- cfgs/standalone_base_trdemu/trunk/winbuild.bat
+++ cfgs/standalone_base_trdemu/trunk/winbuild.bat
@@ -1,17 +1,17 @@
 @ECHO OFF
 rem set PATH=c:\mingw\avrgcc\bin;%PATH%
 rem set PATH=c:\mingw\avrgcc\bin;c:\mingw\bin;c:\mingw\msys\bin
-set PATH=c:\mingw\avrgcc\bin;C:\Windows\system32;C:\Windows;C:\Windows\SysWOW64
+rem set PATH=c:\mingw\avrgcc\bin;C:\Windows\system32;C:\Windows;C:\Windows\SysWOW64
 
-set FPGA_PATH=fpga/quartus
+set FPGA_PATH=..\..\..\fpga\base_trdemu\trunk\quartus
 set FPGA_FILE=top.rbf
 
-set AVR_PATH=avr/build
+set AVR_PATH=..\..\..\avr\baseconf\trunk\build
 set AVR_FILE=top.mlz
 
 set RESULT=zxevo_fw.bin
 
-set MHMT_BIN="../../../tools/mhmt/mhmt.exe"
+set MHMT_BIN="..\..\..\tools\mhmt\mhmt.exe"
 
 IF NOT EXIST %FPGA_PATH%/%FPGA_FILE% (
         ECHO top.rbf not found
@@ -24,5 +24,5 @@ IF NOT EXIST %FPGA_PATH%/%FPGA_FILE% (
 %MHMT_BIN% -maxwin2048 %FPGA_PATH%/%FPGA_FILE% %AVR_PATH%/%AVR_FILE%
 
 make -C %AVR_PATH%
-rem copy avr\build\zxevo.bin zxevo_fw.bin
-copy zxevo_fw.bin e:\zxevo_fw.bin
+copy /Y %AVR_PATH%\zxevo.bin %RESULT%
+rem copy zxevo_fw.bin e:\zxevo_fw.bin
