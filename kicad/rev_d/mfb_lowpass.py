bc2dd24da8a2e670b500f35decd28e29e2652aa6
lvd <lvd@c6c76956-fad2-4150-a3a9-ea0a427205ba>
2025-05-18 20:33:26 +0000

    pentevo: rev D: mfb_lowpass.py: make range selector from preferred values list
    git-svn-id: svn://localhost/local-pentevo@1235 c6c76956-fad2-4150-a3a9-ea0a427205ba

--- kicad/rev_d/mfb_lowpass.py
+++ kicad/rev_d/mfb_lowpass.py
@@ -2,9 +2,11 @@
 
 import sys
 from sympy import *
+import math
+import cmath
 
-e24 = [1.0, 1.1, 1.2, 1.3, 1.5, 1.6, 1.8, 2.0, 2.2, 2.4, 2.7, 3.0, 3.3, 3.6, 3.9, 4.3, 4.7, 5.1, 5.6, 6.2, 6.8, 7.5, 8.2, 9.1];
-e6  = [1.0,                1.5,                2.2,                3.3,                4.7,                6.8               ];
+#e24 = [1.0, 1.1, 1.2, 1.3, 1.5, 1.6, 1.8, 2.0, 2.2, 2.4, 2.7, 3.0, 3.3, 3.6, 3.9, 4.3, 4.7, 5.1, 5.6, 6.2, 6.8, 7.5, 8.2, 9.1];
+#e6  = [1.0,                1.5,                2.2,                3.3,                4.7,                6.8               ];
 
 
 
@@ -104,8 +106,30 @@ def calc_mfb_lowpass():
     print('')
     pprint(Eq(r3,rc_expr[r3]))
     print('')
+    pprint(s_eq1)
+    print('')
+    pprint(s_eq2)
+    print('')
+    pprint(s_eq3)
 
+# these formulae calculated symbolically by the function above and are set here in comments
+# as reference for further numerical calculations.
 """
+         ____________
+       ╲╱ c₁⋅c₂⋅r₂⋅r₃ 
+q = ─────────────────────
+     c₁⋅((1 - k)⋅r₃ + r₂) 
+
+            1             
+wc = ───────────────
+       ____________
+     ╲╱ c₁⋅c₂⋅r₂⋅r₃ 
+
+
+    -r₂ 
+k = ────
+     r₁ 
+
                   2        
             k⋅q⋅wc         
 h = ───────────────────────
@@ -123,10 +147,202 @@ r₃ = ───────────────
 """
 
 
+def select_pvalues_list(values='e24'):
+# returns [1.0, ..., 9.1] list (for e24)
+
+    if values=='e24':
+        r = [1.0, 1.1, 1.2, 1.3, 1.5, 1.6, 1.8, 2.0, 2.2, 2.4, 2.7, 3.0, 3.3, 3.6, 3.9, 4.3, 4.7, 5.1, 5.6, 6.2, 6.8, 7.5, 8.2, 9.1]
+    elif values=='e12':
+        r = [1.0,      1.2,      1.5,      1.8,      2.2,      2.7,      3.3,      3.9,      4.7,      5.6,      6.8,      8.2     ]
+    elif values=='e6':
+        r = [1.0,                1.5,                2.2,                3.3,                4.7,                6.8               ]
+    else:
+        sys.stderr.write('wrong argument given!\n')
+        raise NameError('stop here')
+        sys.exit(1)
+
+    r = [r[-1]*0.1] + r + [r[0]*10.0]
+
+    return r
+
+
+
+
+def select_two_nearest(val,val_exp,pvalues_name='e24'):
+
+    def my_lt(v1,v2):
+        return (v1<v2) and (not math.isclose(v1,v2,rel_tol=1e-3))
+
+    def my_gt(v1,v2):
+        return (v1>v2) and (not math.isclose(v1,v2,rel_tol=1e-3))
+
+
+    pvalues = select_pvalues_list(pvalues_name)
+
+    # select range
+    if my_lt(val, min(pvalues[1:-1])):
+        while my_lt(val, min(pvalues[1:-1])):
+            if my_lt(val, max(pvalues[1:-1])*0.1):
+                val     = val     * 10.0;
+                val_exp = val_exp *  0.1;
+            else:
+                break;
+
+    elif my_gt(val, max(pvalues[1:-1])):
+        while my_gt(val, max(pvalues[1:-1])):
+            if my_gt(val, min(pvalues[1:-1])*10.0):
+                val     = val     *  0.1;
+                val_exp = val_exp * 10.0;
+            else:
+                break;
+
+
+    # select nearest values
+    for i in range(len(pvalues)-1):
+        vdn = pvalues[i];
+        vup = pvalues[i+1];
+        if vdn<=val and val<=vup:
+            break;
+
+    vdn_exp = val_exp
+    vup_exp = val_exp
+
+    if vdn<pvalues[1]:
+        vdn *= 10.0
+        vdn_exp *= 0.1
+
+    if vup>pvalues[-2]:
+        vup *= 0.1
+        vup_exp *= 10.0
+
+    return [ (vdn,vdn_exp), (vup,vup_exp) ];
+
+def get_neighbour_value(val,dir='up',pvalues_name='e24'):
+    
+    pvalues = select_pvalues_list(pvalues_name)
+    exp = 1.0
+
+    found_v = None
+    found_idx = None
+    for idx,v in enumerate(pvalues[1:-1]):
+        if math.isclose(val,v,rel_tol=1e-3):
+            found_v = v
+            found_idx = idx
+
+            print(' found {} {}'.format(found_v,found_idx))
+            break
+    
+    if found_v is None:
+        raise NameError('found_v is None!')
+        sys.exit(1)
+
+    if dir=='up':
+        if math.isclose(found_v,pvalues[-2],rel_tol=1e-3):
+            next_v = pvalues[1]
+            exp = 10.0
+        else:
+            tmp = pvalues[1:-1]
+            print('  tmp {}'.format(tmp))
+            next_v = tmp[found_idx+1]
+        
+    elif dir=='down':
+        if math.isclose(found_v,pvalues[1],rel_tol=1e-3):
+            next_v = pvalues[-2]
+            exp = 0.1
+        else:
+            tmp = pvalues[1:-1]
+            next_v = tmp[found_idx-1]
+
+    else:
+        raise NameError('dir is neither "up" nor "down"!')
+        sys.exit(1)
+
+    return (next_v,exp)
+
+
+def val_exp_le( val1, val1_exp, val2, val2_exp ):
+
+    return (val1_exp < val2_exp) or ( math.isclose(val1_exp, val2_exp, rel_tol=1e-3) and ( (val1 < val2) or math.isclose(val1, val2, rel_tol=1e-3) ) )
+
+
+def select_values_in_range(vrange, vrange_exp, pvalues='e24'):
+# range is [min,max]
+# returns list of values already multiplied by exp
+
+    vmin=vrange[0]
+    vmax=vrange[1]
+
+    if vmax<=vmin:
+        raise NameError('wrong vrange: [0] must be bigger than [1]')
+        sys.exit(1)
+
+    # expand interval by nearest preferred values
+    vmin_vals = select_two_nearest(vmin,vrange_exp, pvalues_name=pvalues)
+    vmax_vals = select_two_nearest(vmax,vrange_exp, pvalues_name=pvalues)
+    print(vmin,vmax,vrange_exp)
+    print(vmin_vals,vmax_vals)
+
+
+    # start building values list
+
+    vmin_val_0, vmin_exp_0 = vmin_vals[0]
+    vmin_val_1, vmin_exp_1 = vmin_vals[1]
+    vmax_val_0, vmax_exp_0 = vmax_vals[0]
+    vmax_val_1, vmax_exp_1 = vmax_vals[1]
+
+    if math.isclose(vmin_val_1*vmin_exp_1, vmin*vrange_exp, rel_tol=1e-3):
+        curr_v, curr_exp = vmin_vals[1]
+    else:
+        curr_v, curr_exp = vmin_vals[0]
+
+    if math.isclose(vmax_val_0*vmax_exp_0, vmax*vrange_exp, rel_tol=1e-3):
+        vmax_v, vmax_exp = vmax_vals[0]
+    else:
+        vmax_v, vmax_exp = vmax_vals[1]
+
+    values_range = []
+
+    print('vmaxs: {} {}'.format(vmax_v, vmax_exp))
+    while val_exp_le( curr_v, curr_exp, vmax_v, vmax_exp ):
+      
+        print('currs: {} {}'.format(curr_v, curr_exp))
+
+        values_range = values_range + [ (curr_v, curr_exp) ]
+
+        next_v, exp_update = get_neighbour_value( curr_v, dir='up', pvalues_name=pvalues )
+        
+        print('nexts: {} {}\n'.format(next_v, exp_update))
+
+        curr_v = next_v
+        curr_exp *= exp_update
+
+    
+    return values_range
+
+
+
+
+def calc_filter( wc_set=2*math.pi*30000, q_set=1.0, k_set=(-1.17), r1_range=[20.0,30.0], r1_exp=1e3, r1_div=3.0, r1_values='e24', c1_range=[47.0,10000.0], c1_exp=1e-12, c1_values='e6'):
+
+    c1_values = select_values_in_range( c1_range, c1_exp, pvalues=c1_values )
+
+    r1_values = select_values_in_range( [x for x in r1_range], r1_exp, pvalues=r1_values )
+
+    print( c1_values )
+    print( r1_values )
+
+
+
+
+
+
 
 def main():
 
     #calc_mfb_lowpass()
+
+    calc_filter()
+
 
 
 if __name__=="__main__":
